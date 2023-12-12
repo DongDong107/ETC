7aab0f1c377b29aa28d22fa8010590cd7971b72b
dongdong107 <jkds989@gmail.com>
2023-12-12 17:59:58 +0900

    1212

--- weatherPot/app.js
+++ weatherPot/app.js
@@ -1,1 +1,18 @@
+let latitude;
+let longitude;
+navigator.geolocation.getCurrentPosition((position)=>{
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log(position.coords.latitude, position.coords.longitude);
+})
 
+console.log(latitude, longitude);
+
+const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=JEhDmCwhRn8qnYcdy51GNHZ73w%2FOjY25glFUFhUYizJfsdJVcbmRqYTBihA87eMuYrovbfBWPiDRF1bQ0dANzw%3D%3D&pageNo=1&numOfRows=1000&dataType=json&base_date=20231212&base_time=0500&nx=60&ny=127`;
+
+let rData;
+let wData = fetch(url)
+  .then((response) => response.json())
+  .then((data) => console.log(JSON.parse(data).body));
+
+console.log(wData);
